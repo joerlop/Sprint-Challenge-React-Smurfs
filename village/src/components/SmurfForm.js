b702c55c8b8e38a49a4a5fb75d37cba2383c6a8d
joerlop <jj.erlich155@gmail.com>
2019-05-10 10:32:13 -0500

    First commit. POST and GET done

--- village/src/components/SmurfForm.js
+++ village/src/components/SmurfForm.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+import axios from "axios";
+
 class SmurfForm extends Component {
   constructor(props) {
     super(props);
@@ -13,7 +15,22 @@ class SmurfForm extends Component {
   addSmurf = event => {
     event.preventDefault();
     // add code to create the smurf using the api
-
+    const smurf = {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height      
+    }
+    axios.post("http://localhost:3333/smurfs", {
+      name: this.state.name,
+      age: this.state.age,
+      height: this.state.height  
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });   
     this.setState({
       name: '',
       age: '',
